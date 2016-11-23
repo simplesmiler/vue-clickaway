86dbc3606ea0929b56959539f58efb612331dd48
Denis Karabaza <denis.karabaza@gmail.com>
2016-11-24 00:00:34 +0300

    Fix "origin" click triggering the away callback

--- index.js
+++ index.js
@@ -28,10 +28,22 @@ function bind(el, binding) {
     return;
   }
 
+  // @NOTE: Vue binds directives in microtasks, while UI events are dispatched
+  //        in macrotasks. This causes the listener to be set up before
+  //        the "origin" click event (the event that lead to the binding of
+  //        the directive) arrives at the document root. To work around that,
+  //        we ignore events until the end of the "initial" macrotask.
+  // @REFERENCE: https://jakearchibald.com/2015/tasks-microtasks-queues-and-schedules/
+  // @REFERENCE: https://github.com/simplesmiler/vue-clickaway/issues/8
+  var initialMacrotaskEnded = false;
+  setTimeout(function() {
+    initialMacrotaskEnded = true;
+  }, 0);
+
   el[HANDLER] = function(ev) {
     // @NOTE: IE 5.0+
     // @REFERENCE: https://developer.mozilla.org/en/docs/Web/API/Node/contains
-    if (!el.contains(ev.target)) {
+    if (initialMacrotaskEnded && !el.contains(ev.target)) {
       return callback(ev);
     }
   };
