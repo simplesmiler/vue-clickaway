5e9482ef1c10e08d56fd4eed7f9cd6f6855e4bda
txs1992 <xusheng.tao@ele.me>
2019-03-11 20:37:01 +0800

    fix: class array indexOf error

--- dist/vue-clickaway.common.js
+++ dist/vue-clickaway.common.js
@@ -52,7 +52,7 @@ function bind(el, binding, vnode) {
     //        to the top.
     // @NOTE: `.path` is non-standard, the standard way is `.composedPath()`
     var path = ev.path || (ev.composedPath ? ev.composedPath() : undefined);
-    if (initialMacrotaskEnded && (path ? path.indexOf(el) < 0 : !el.contains(ev.target))) {
+    if (initialMacrotaskEnded && (path ? Array.from(path).indexOf(el) < 0 : !el.contains(ev.target))) {
       return callback.call(vm, ev);
     }
   };
